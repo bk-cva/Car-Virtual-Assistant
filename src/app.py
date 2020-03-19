5d9dc8c8dbb04454d17582f8240da41de37aef4d
Vũ Đức Duy <duyisking1060@gmail.com>
2020-03-19 16:48:38 +0700

    Integrate protobuf

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 import logging
-
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
+from google.protobuf import json_format
 
 from src.nlp import predict_intent, predict_entity
+from src.proto import rest_api_pb2
 
 app = Flask(__name__)
 gunicorn_logger = logging.getLogger('gunicorn.error')
@@ -14,12 +15,16 @@ CORS(app)
 
 @app.errorhandler(404)
 def url_error(e):
-    return make_response(jsonify({'message': str(e)}), 404)
+    response = rest_api_pb2.Response()
+    response.error.error_message = str(e)
+    return make_response(json_format.MessageToJson(response), 404)
 
 
 @app.errorhandler(500)
 def server_error(e):
-    return make_response(jsonify({'message': str(e)}), 500)
+    response = rest_api_pb2.Response()
+    response.error.error_message = str(e)
+    return make_response(json_format.MessageToJson(response), 500)
 
 
 @app.route('/healthz', methods=['GET'])
@@ -33,9 +38,15 @@ def health_check_route():
 def analyze():
     body = request.get_json()
     texts = body['texts']
-    results = [{
-        'intent': predict_intent(text),
-        'entity': predict_entity(text)
-    } for text in texts]
+    results = []
+    for text in texts:
+        predict_result = rest_api_pb2.PredictResult()
+        intent = predict_intent(text)
+        predict_result.intent_id = intent
+        predict_result.intent = intent.name
+        predict_result.entity.extend(predict_entity(text))
+        results.append(predict_result)
 
-    return make_response(jsonify({'results': results}), 200)
+    response = rest_api_pb2.Response()
+    response.results.extend(results)
+    return make_response(json_format.MessageToJson(response), 200)
--- src/nlp/entities/ner.py
+++ src/nlp/entities/ner.py
@@ -1,34 +1,50 @@
 import re
-from typing import Dict
+from typing import List
 
 from .constants import PLACE_REGEX, STREET_REGEX, WARD_REGEX, DISTRICT_REGEX
+from src.proto.rest_api_pb2 import Entity
 
 
-def predict_entity(text: str) -> Dict:
-    results = {}
+def predict_entity(text: str) -> List[Entity]:
+    results = []
     for place in PLACE_REGEX:
         pattern = '%s (.+)' % place
         if re.search(pattern, text, re.IGNORECASE):
-            results['place'] = place
-            results['place_name'] = re.search(pattern, text, re.IGNORECASE).group(1)
+            entity = Entity()
+            entity.name = 'place'
+            entity.value = place
+            entity2 = Entity()
+            entity2.name = 'place_name'
+            entity2.value = re.search(pattern, text, re.IGNORECASE).group(1)
+            results.append(entity)
+            results.append(entity2)
             break
 
     for street in STREET_REGEX:
         pattern = '%s (.+)' % street
         if re.search(pattern, text, re.IGNORECASE):
-            results['street'] = re.search(pattern, text, re.IGNORECASE).group(1)
+            entity = Entity()
+            entity.name = 'street'
+            entity.value = re.search(pattern, text, re.IGNORECASE).group(1)
+            results.append(entity)
             break
 
     for ward in WARD_REGEX:
         pattern = '%s (.+)' % ward
         if re.search(pattern, text, re.IGNORECASE):
-            results['ward'] = re.search(pattern, text, re.IGNORECASE).group(1)
+            entity = Entity()
+            entity.name = 'ward'
+            entity.value = re.search(pattern, text, re.IGNORECASE).group(1)
+            results.append(entity)
             break
 
     for district in DISTRICT_REGEX:
         pattern = '%s (.+)' % district
         if re.search(pattern, text, re.IGNORECASE):
-            results['district'] = re.search(pattern, text, re.IGNORECASE).group(1)
+            entity = Entity()
+            entity.name = 'district'
+            entity.value = re.search(pattern, text, re.IGNORECASE).group(1)
+            results.append(entity)
             break
 
     return results
