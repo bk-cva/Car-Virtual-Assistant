32991431c20adc8fe86419d0fe52291370b66c0b
Vũ Đức Duy <duyisking1060@gmail.com>
2020-03-19 17:25:22 +0700

    Update regex entities

--- src/nlp/entities/constants.py
+++ src/nlp/entities/constants.py
@@ -1,4 +1,14 @@
-PLACE_REGEX = ['bệnh viện', 'trường', 'chung cư', 'cao ốc', 'quán', 'nhà hàng', 'tiệm', 'cửa hàng']
-STREET_REGEX = ['đường']
-WARD_REGEX = ['phường']
-DISTRICT_REGEX = ['quận']
+PHONE_CALL_REGEX = [
+    '^gọi điện thoại cho ([\w\s]+)',
+    '^gọi điện thoại tới ([\w\s]+)',
+    '^gọi điện cho ([\w\s]+)',
+    '^gọi điện tới ([\w\s]+)',
+    '^gọi cho ([\w\s]+)',
+    '^gọi tới ([\w\s]+)',
+    '^gọi ([\w\s]+)',
+]
+PHONE_TEXT_REGEX = [
+    '^nhắn tin cho ([\w\s]+) rằng ([\w\s]+)',
+    '^nhắn cho ([\w\s]+) rằng ([\w\s]+)',
+    '^nhắn ([\w\s]+) rằng ([\w\s]+)',
+]
--- src/nlp/entities/ner.py
+++ src/nlp/entities/ner.py
@@ -1,50 +1,35 @@
 import re
 from typing import List
 
-from .constants import PLACE_REGEX, STREET_REGEX, WARD_REGEX, DISTRICT_REGEX
+from .constants import PHONE_CALL_REGEX, PHONE_TEXT_REGEX
 from src.proto.rest_api_pb2 import Entity
 
 
 def predict_entity(text: str) -> List[Entity]:
     results = []
-    for place in PLACE_REGEX:
-        pattern = '%s (.+)' % place
-        if re.search(pattern, text, re.IGNORECASE):
+    for pattern in PHONE_CALL_REGEX:
+        search_res = re.search(pattern, text, re.IGNORECASE)
+        if search_res:
             entity = Entity()
-            entity.name = 'place'
-            entity.value = place
+            entity.name = 'person_name'
+            entity.value = search_res.group(1)
+
+            results.append(entity)
+            break
+
+    for pattern in PHONE_TEXT_REGEX:
+        search_res = re.search(pattern, text, re.IGNORECASE)
+        if search_res:
+            entity = Entity()
+            entity.name = 'person_name'
+            entity.value = search_res.group(1)
+
             entity2 = Entity()
-            entity2.name = 'place_name'
-            entity2.value = re.search(pattern, text, re.IGNORECASE).group(1)
+            entity2.name = 'message'
+            entity2.value = search_res.group(2)
+
             results.append(entity)
             results.append(entity2)
-            break
-
-    for street in STREET_REGEX:
-        pattern = '%s (.+)' % street
-        if re.search(pattern, text, re.IGNORECASE):
-            entity = Entity()
-            entity.name = 'street'
-            entity.value = re.search(pattern, text, re.IGNORECASE).group(1)
-            results.append(entity)
-            break
-
-    for ward in WARD_REGEX:
-        pattern = '%s (.+)' % ward
-        if re.search(pattern, text, re.IGNORECASE):
-            entity = Entity()
-            entity.name = 'ward'
-            entity.value = re.search(pattern, text, re.IGNORECASE).group(1)
-            results.append(entity)
-            break
-
-    for district in DISTRICT_REGEX:
-        pattern = '%s (.+)' % district
-        if re.search(pattern, text, re.IGNORECASE):
-            entity = Entity()
-            entity.name = 'district'
-            entity.value = re.search(pattern, text, re.IGNORECASE).group(1)
-            results.append(entity)
             break
 
     return results
